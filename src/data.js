e7276be12413674811b63e04d80b885573dae15a
Muhammad Alyan <alyan0332@gmail.com>
2022-07-10 09:33:40 +0500

    fix images not showing

--- src/data.js
+++ src/data.js
@@ -1,30 +1,36 @@
+// images
+import samsung from "./images/samsung.png";
+import iphone from "./images/iphone.jpg";
+import googlePixle from "./images/google-pixle.jpg";
+import oneplus from "./images/oneplus.png";
+
 export const data = [
   {
     id: "rec1JZlfCIBOPdcT2",
     title: "Galaxy S22 Ultra",
     price: 1199,
-    img: "../images/samsung.png",
+    img: samsung,
     amount: 1,
   },
   {
     id: "recB6qcHPxb62YJ75",
     title: "Iphone 13 pro",
     price: 999,
-    img: "../images/iphone.jpg",
+    img: iphone,
     amount: 1,
   },
   {
     id: "recdRxBsE14Rr2VuJ",
     title: "Google Pixle 6",
     price: 599,
-    img: "../images/google-pixle.jpg",
+    img: googlePixle,
     amount: 1,
   },
   {
     id: "recwTo160XST3PIoW",
     title: "Oneplus Nord N20",
     price: 282,
-    img: "../images/oneplus.png",
+    img: oneplus,
     amount: 1,
   },
 ];
