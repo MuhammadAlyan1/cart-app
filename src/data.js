bb2c23bdb5a1cf159927a3740af8a7919c4064f5
Muhammad Alyan <alyan0332@gmail.com>
2022-08-20 16:39:46 +0500

    place url instead of image

--- src/data.js
+++ src/data.js
@@ -1,36 +1,30 @@
-// images
-import samsung from "./images/samsung.png";
-import iphone from "./images/iphone.jpg";
-import googlePixle from "./images/google-pixle.jpg";
-import oneplus from "./images/oneplus.png";
-
 export const data = [
   {
-    id: "rec1JZlfCIBOPdcT2",
-    title: "Galaxy S22 Ultra",
+    id: 'rec1JZlfCIBOPdcT2',
+    title: 'Galaxy S21 Ultra',
     price: 1199,
-    img: samsung,
+    img: 'https://images.unsplash.com/photo-1610945415295-d9bbf067e59c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2Ftc3VuZ3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60',
     amount: 1,
   },
   {
-    id: "recB6qcHPxb62YJ75",
-    title: "Iphone 13 pro",
+    id: 'recB6qcHPxb62YJ75',
+    title: 'Iphone 13 pro',
     price: 999,
-    img: iphone,
+    img: 'https://images.unsplash.com/photo-1633053699034-459674171bec?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTB8fGlwaG9uZSUyMDEzJTIwcHJvfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
     amount: 1,
   },
   {
-    id: "recdRxBsE14Rr2VuJ",
-    title: "Google Pixle 6",
+    id: 'recdRxBsE14Rr2VuJ',
+    title: 'Iphone 6',
     price: 599,
-    img: googlePixle,
+    img: 'https://images.unsplash.com/photo-1555375771-14b2a63968a9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjN8fHNtYXJ0JTIwcGhvbmV8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
     amount: 1,
   },
   {
-    id: "recwTo160XST3PIoW",
-    title: "Oneplus Nord N20",
+    id: 'recwTo160XST3PIoW',
+    title: 'Oneplus Nord N20',
     price: 282,
-    img: oneplus,
+    img: 'https://images.unsplash.com/photo-1636376138385-b41caa8e0ae0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTUyfHxzbWFydCUyMHBob25lfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60',
     amount: 1,
   },
 ];
